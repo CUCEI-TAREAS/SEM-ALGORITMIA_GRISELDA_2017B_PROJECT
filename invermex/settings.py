b1a9c9f207b6a358cd597dfd689ba0871d87bbeb
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2017-11-26 12:39:30 -0600

    loading static files as a template from index.html
    run the server as python manage.py runserver

--- invermex/settings.py
+++ invermex/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    "/var/www/invermex/invermex/static",
+    '/var/www/static/',
+]
