48c2ff6683f87e6ae6694640869fd241dd8f0718
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2017-11-27 18:37:01 -0600

    loading base template with global variables
    creating global variable and feeding it to load on template base.html
    
    BUG :
    	BASE_MODAL_LINKS_DICTS is showed incorrectly from template base.html

--- invermex/views.py
+++ invermex/views.py
@@ -2,12 +2,19 @@ from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
 
+BASE_MODAL_LINKS_DICT = {'text' : "INICIAR SESSION", 'url' : "login.html" }
+BASE_MODAL_LINKS_LIST = [ BASE_MODAL_LINKS_DICT ]
+
+BASE = {
+    'BASE_TITLE' : "INVERMEX",
+    'BASE_MAIN_TITLE' : "INVIERTE SEGURO",
+    'BASE_MODAL_LINKS_LIST' : BASE_MODAL_LINKS_LIST
+}
+
 def index(request):
-    t = get_template('index.html')
-    html = t.render()
-    return HttpResponse(html)
+    t = get_template('base.html').render(BASE)
+    return HttpResponse(t)
 
 def register_client(request):
-    t = get_template('register_client.html')
-    html = t.render()
-    return HttpResponse(html)
+    t = get_template('register_client.html').render()
+    return HttpResponse(t)
